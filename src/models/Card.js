343168b02e184e410078ab8322c4f40d34cea1e8
RafaelZapata <faelzapata2011@gmail.com>
2023-03-17 06:19:02 -0300

    fix card fields bugs

--- src/models/Card.js
+++ src/models/Card.js
@@ -11,7 +11,7 @@ const cardSchema = Schema({
     type: {type: mongoose.Schema.Types.ObjectId, ref: "types"},
     rarity: {type: mongoose.Schema.Types.ObjectId, ref: "rarities"},
     card_class: {type: mongoose.Schema.Types.ObjectId, ref: "classes"},
-    subclasses: {type: mongoose.Schema.Types.ObjectId, ref: "subclasses"},
+    subclass: {type: mongoose.Schema.Types.ObjectId, ref: "subclasses"},
     effect: {type: String},
     params: {type: String},
 })
