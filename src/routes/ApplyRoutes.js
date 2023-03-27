2f2ad9800dbd457e08201ad50f7dbff179e3b4c7
Rafael Zapata <faelzapata2011@gmail.com>
2023-03-26 21:19:38 -0300

    Fix apply route bug

--- src/routes/ApplyRoutes.js
+++ src/routes/ApplyRoutes.js
@@ -2,7 +2,6 @@ import cardRoutes from "./CardRoutes.js";
 import classRoutes from "./ClassRoutes.js";
 import deckRoutes from "./DeckRoutes.js";
 import effectRoutes from "./EffectRoutes.js";
-import heroEffectRoutes from "./HeroEffectRoutes.js";
 import heroRoutes from "./HeroRoutes.js";
 import matchRoutes from "./MatchRoutes.js";
 import rarityRoutes from "./RarityRoutes.js";
